267434c1f7725d2856ab454f1bec0ddd8794762c
Cesar <cesarcm22@gmail.com>
2019-01-17 22:56:00 -0800

    Primera Entrada

---
+++ ProyectoXamarinCCM/ProyectoXamarinCCM/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProyectoXamarinCCM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            BotonArtsTemporada.Clicked += BotonArtsTemporada_Clicked;
+
+            BotonPromociones.Clicked += BotonPromociones_Clicked;
+        }
+
+        private void BotonPromociones_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ArtsPromociones());
+        }
+
+        private void BotonArtsTemporada_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ArtsTemporada());
+        }
+    }
+}
