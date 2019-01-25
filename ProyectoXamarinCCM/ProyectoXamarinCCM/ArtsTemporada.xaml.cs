d9334e66e11d5207564aa080c266587afe5eab3f
Cesar <cesarcm22@gmail.com>
2019-01-24 23:25:30 -0800

    Subir Proyecto

--- ProyectoXamarinCCM/ProyectoXamarinCCM/ArtsTemporada.xaml.cs
+++ ProyectoXamarinCCM/ProyectoXamarinCCM/ArtsTemporada.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 //using ProyectoXamarinCCM.Modelos;
+
 using ProyectoXamarinCCM.Servicios;
 using Plugin.Connectivity;
 using ProyectoXamarinCCM.Modelos;
